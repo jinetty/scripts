77d1c131c82f745e0ff4a33c3b6029e44f3c17a2
Ümit Taylan <64617882+jinetty@users.noreply.github.com>
2024-04-22 17:53:37 +0300

    I made a big mistake behind this logic

--- check_local_git_repositories.sh
+++ check_local_git_repositories.sh
@@ -2,7 +2,7 @@
 
 directories=""
 [[ -z $1 ]] && echo "You need to specify a path to list directories." && exit
-if [[ $(stat -c '%u' $1) -eq $UID ]] && [[ $(stat -c '%a' $1) -gt 400 ]]; then
+if [[ $(stat -c '%u' $1) -ge $UID ]] && [[ $(stat -c '%a' $1) -gt 400 ]]; then
     for directory in $(find $1 -type d -name ".git" 2>/dev/null |sed 's/.git//g')
     do
         directories="$directories $directory\n"
